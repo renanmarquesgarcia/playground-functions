3df2b2e0cde8490205c1a8077554084bc4f5d854
Renan Marques <nan.dev01@gmail.com>
2022-09-28 12:51:12 -0300

    Desafio 12 resolvido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,18 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  return numbers.join();
+
 }
 
+
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  
+  if(lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB && lineA > (Math.abs(lineB - lineC)) && lineB > (Math.abs(lineA - lineC)) && lineC > (Math.abs(lineA - lineB))){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 13
