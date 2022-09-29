4629f07395ac7cfa8185f00a5d462a53023f004f
Renan Marques <nan.dev01@gmail.com>
2022-09-29 11:05:18 -0300

    Desafio 13 resolvido

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,7 +4,6 @@ function generatePhoneNumber(numbers) {
 
 }
 
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   
@@ -16,9 +15,27 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  
+  let reg = /\d+/g; // dentro das barras (/), eu defino minha expressão regular; d = digitos(números); + significa 1 ou mais; g é uma bandeira dizendo que eu quero todos os dígitos 
+  let numbersArrayString = (string.match(reg));
+  let numbersArray = []
+  let sum = 0;
+
+  for(let index = 0; index < numbersArrayString.length; index += 1){
+    numbersArray.push(parseInt(numbersArrayString[index]));
+  }
+  for(let index = 0; index < numbersArray.length; index += 1){
+    sum += numbersArray[index]  
+  }
+    if (sum === 1){
+      return sum + ' copo de água';
+    } else {
+      return sum + ' copos de água';
+    }
 }
+
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
